9a61b5bce80cc1747e31a0a5a87d1b4ad5ab2129
Kayaba-Attribution <63566185+Kayaba-Attribution@users.noreply.github.com>
2020-04-12 20:00:39 -0400

    I forgot to import random

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 guess_count = 0
 random_number = random.randint(0, 21)
 print("Guess the random number between 1 and 20 to win! You have 5 guesses")
@@ -13,14 +12,16 @@ while guess_count <= 5:
     if guess == random_number:
         print('That is correct. You win!')
         break
-    elif 0 < guess < random_number < 21:
-        print("Too low. Try again.")
-        guess_count += 1
-    elif 21 > guess > random_number > 0:
-        print("Too high. Try again.")
-        guess_count += 1
     elif guess not in possible_guesses:
         print('Number is between 1 and 20. Try again.')
+        guess_count += 1
+    elif guess < random_number:
+        print("Too low. Try again.")
+        guess_count += 1
+    elif guess > random_number:
+        print("Too high. Try again.")
+        guess_count += 1
+    
 if guess != random_number:
     print(f'I am sorry but you lost. The number was {random_number}.')
 else:
